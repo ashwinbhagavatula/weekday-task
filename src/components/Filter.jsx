d3d698bd1b630cc19c4a3e132825738d8460456d
ashwinbhagavatula <ashwinbhagavatula02@gmail.com>
2024-05-04 18:29:39 +0530

    added options for filters

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Select from 'react-select';
-
+import { TextField } from '@mui/material';
 
 const roles = [
   {value:'backend', label: 'Backend'},
@@ -46,11 +46,51 @@ const roles = [
   {value:'legal', label: 'Legal'},
   {value:'hr', label: 'HR'},
   {value:'finance', label: 'Finance'},
-  
 ];
+
+const noOfEmployees = [
+  {value:'1-10', label: '1-10'},
+  {value:'11-20', label: '11-20'},
+  {value:'21-50', label: '21-50'},
+  {value:'51-100', label: '51-100'},
+  {value:'101-200', label: '101-200'},
+  {value:'201-500', label: '201-500'},
+  {value:'500+', label: '500+'},
+];
+
+const exp = [
+  {value:'1', label: '1'},
+  {value:'2', label: '2'},
+  {value:'3', label: '3'},
+  {value:'4', label: '4'},
+  {value:'5', label: '5'},
+  {value:'6', label: '6'},
+  {value:'7', label: '7'},
+  {value:'8', label: '8'},
+  {value:'9', label: '9'},
+  {value:'10', label: '10'},
+];
+
+const jobMode = [
+  {value:'remote', label: 'Remote'},
+  {value:'hybrid', label: 'Hybrid'},
+  {value:'in-office', label: 'In-Office'},
+]
+
+const salaries = [
+  {value:'0', label: '0L'},
+  {value:'10', label: '10L'},
+  {value:'20', label: '20L'},
+  {value:'30', label: '30L'},
+  {value:'40', label: '40L'},
+  {value:'50', label: '50L'},
+  {value:'60', label: '60L'},
+  {value:'70', label: '70L'},
+
+]
 function Filter() {
   return (
-    <div>
+    <div className='filter'>
       <Select
       // defaultValue={[colourOptions[2], colourOptions[3]]}
       isMulti
@@ -58,6 +98,55 @@ function Filter() {
       options={roles}
       className="basic-multi-select"
       classNamePrefix="select"
+      placeholder={<div className="select-placeholder-text">Roles</div>}
+      />
+      <Select
+      // defaultValue={[colourOptions[2], colourOptions[3]]}
+      isMulti
+      name="colors"
+      options={noOfEmployees}
+      className="basic-multi-select"
+      classNamePrefix="select"
+      placeholder={<div className="select-placeholder-text">Number Of Employees</div>}
+      />
+      <Select
+      // defaultValue={[colourOptions[2], colourOptions[3]]}
+      isMulti
+      name="colors"
+      options={exp}
+      className="basic-multi-select"
+      classNamePrefix="select"
+      placeholder={<div className="select-placeholder-text">Experience</div>}
+      />
+      <Select
+      // defaultValue={[colourOptions[2], colourOptions[3]]}
+      isMulti
+      name="colors"
+      options={jobMode}
+      className="basic-multi-select"
+      classNamePrefix="select"
+      placeholder={<div className="select-placeholder-text">Remote</div>}
+      />
+      <Select
+      // defaultValue={[colourOptions[2], colourOptions[3]]}
+      isMulti
+      name="colors"
+      options={salaries}
+      className="basic-multi-select"
+      classNamePrefix="select"
+      placeholder={<div className="select-placeholder-text">Minimum Base Pay Salary</div>}
+      />
+      
+      <TextField id="outlined-basic" 
+      label="Search Company Name" 
+      variant="outlined"
+      InputProps={{ style: { height: '38px'} }}
+      
+      sx={{
+        '& .MuiInputLabel-root': {
+          top: '-20%',
+        }
+      }}
       />
     </div>
   )
